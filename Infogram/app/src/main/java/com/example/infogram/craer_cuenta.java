738656e71be5555d2869c83b5865a2856c90cd2c
VFzambrana <zambranafabrizio@gmail.com>
2021-08-19 16:30:33 -0400

    Formulario

---
+++ Infogram/app/src/main/java/com/example/infogram/craer_cuenta.java
@@ -0,0 +1,14 @@
+package com.example.infogram;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class craer_cuenta extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_craer_cuenta);
+    }
+}
