a189ac39eebe22283edaf4f1677cb53c1088a180
KaOSrp19 <kevinramos28101999@hotmail.com>
2021-09-13 19:15:52 -0400

    Fragment

--- Infogram/app/src/main/java/com/example/infogram/Infograms.java
+++ Infogram/app/src/main/java/com/example/infogram/Infograms.java
@@ -6,6 +6,9 @@ import android.view.View;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.example.infogram.View.Container_activity;
+import com.example.infogram.View.crear_cuenta;
+
 public class Infograms extends AppCompatActivity {
 
     @Override
@@ -14,7 +17,14 @@ public class Infograms extends AppCompatActivity {
         setContentView(R.layout.activity_infograms);
     }
     public void goCreateAccount(View view){
-        Intent intent = new Intent(Infograms.this,crear_cuenta.class);
+        //Intent intent = new Intent(Infograms.this,crear_cuenta.class);
+
+        Intent intent = new Intent(this, crear_cuenta.class);
+        startActivity(intent);
+    }
+
+    public void login(View view) {
+        Intent intent = new Intent(this, Container_activity.class);
         startActivity(intent);
     }
 }
