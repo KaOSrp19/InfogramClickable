9dba60bc2f5c4ea41c10920cf9afc91667e61d10
KaOSrp19 <kevinramos28101999@hotmail.com>
2021-09-13 19:25:37 -0400

    Recycler View

--- Infogram/app/src/main/java/com/example/infogram/View/fragment/HomeFragment.java
+++ Infogram/app/src/main/java/com/example/infogram/View/fragment/HomeFragment.java
@@ -5,9 +5,17 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.infogram.R;
+import com.example.infogram.adapter.CardViewAdapter;
+import com.example.infogram.model.Image;
+
+import java.util.ArrayList;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -60,6 +68,49 @@ public class HomeFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false);
+        View view = inflater.inflate(R.layout.fragment_home, container, false);
+
+        //toolbar
+        showToolBar(getString(R.string.title_home_fragment),false,view);
+
+        //recycleview
+        RecyclerView recyclerView = view.findViewById(R.id.recycle_view);
+
+        //layoud manager
+        LinearLayoutManager linearLayoutManager=new LinearLayoutManager(getContext());
+        linearLayoutManager.setOrientation(LinearLayoutManager.VERTICAL);
+        recyclerView.setLayoutManager(linearLayoutManager);
+
+        //el adapter
+        CardViewAdapter cardViewAdapter = new CardViewAdapter(buidImages(),R.layout.cardview_image,getActivity());
+        recyclerView.setAdapter(cardViewAdapter);
+
+        return view;
+    }
+
+    public void showToolBar(String titulo, boolean botonSubir , View view){
+
+        Toolbar toolbar = view.findViewById(R.id.toolbar);
+        ((AppCompatActivity)getActivity()).setSupportActionBar(toolbar);
+
+        ((AppCompatActivity)getActivity()).getSupportActionBar().setTitle(titulo);
+        ((AppCompatActivity)getActivity()).getSupportActionBar().setDisplayHomeAsUpEnabled(botonSubir);
+    }
+
+    //creamos la lista de imagenes
+    public ArrayList<Image> buidImages(){
+        ArrayList<Image> images = new ArrayList<>();
+
+        images.add(new Image("","Kevin Ramos","2 dias","1 me gusta"));
+        images.add(new Image("","Luis Peres","3 dias","5 me gusta"));
+        images.add(new Image("","Fernando Torres","6 dias","4 me gusta"));
+        images.add(new Image("","Mario Jose","5 dias","6 me gusta"));
+        images.add(new Image("","Miguel Angel","3 dias","2 me gusta"));
+        images.add(new Image("","Alex Moreira","4 dias","5 me gusta"));
+        images.add(new Image("","Ana Dewid","2 dias","3 me gusta"));
+        images.add(new Image("","Antonio Banderas","2 dias","8 me gusta"));
+        images.add(new Image("","Andrea Laura","6 dias","3 me gusta"));
+        images.add(new Image("","Luisa Garsia","9 dias","1 me gusta"));
+        return images;
     }
 }
