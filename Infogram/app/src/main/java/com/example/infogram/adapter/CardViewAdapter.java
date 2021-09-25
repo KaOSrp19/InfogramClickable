e107f37023b7733ddba67a5aad05825eed138d60
KaOSrp19 <kevinramos28101999@hotmail.com>
2021-09-25 15:46:08 -0400

    Transiciones en Android

--- Infogram/app/src/main/java/com/example/infogram/adapter/CardViewAdapter.java
+++ Infogram/app/src/main/java/com/example/infogram/adapter/CardViewAdapter.java
@@ -2,6 +2,8 @@ package com.example.infogram.adapter;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.os.Build;
+import android.transition.Explode;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -9,6 +11,7 @@ import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
+import androidx.core.app.ActivityOptionsCompat;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.infogram.R;
@@ -52,10 +55,24 @@ public class CardViewAdapter extends RecyclerView.Adapter<CardViewAdapter.CardVi
 
         //onclicklistener
         holder.imageCardView.setOnClickListener(new View.OnClickListener() {
+
+
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(activity, ImageDetailActivity.class);
-                activity.startActivity(intent);
+
+                if(Build.VERSION.SDK_INT>=Build.VERSION_CODES.LOLLIPOP){
+                    Explode explode = new Explode();
+                    explode.setDuration(1000);
+                    activity.getWindow().setExitTransition(explode);
+
+                    activity.startActivity(intent, ActivityOptionsCompat.makeSceneTransitionAnimation(activity,v, activity.getString(R.string.transitionName_imageCardView)).toBundle());
+
+                }else {
+                    activity.startActivity(intent);
+                }
+
+
             }
         });
 
